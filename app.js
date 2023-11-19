ff14344aa60be500b97924654124234cd03b67f5
Jakub Rana <kubik.rana@icloud.com>
2023-11-19 15:37:00 +0100

    almost responsive

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// list of exhibitors
 const mapElements = [["Testovací jízdy", 1], ["TVD-Technická výroba, a.s.", 2], ["PHOENIX CONTACT, s.r.o.", 2], ["Ejoin – nabíjecí stanice pro elektromobily", 2], ["SECTRON s.r.o.", 2], ["TPC CITY II – Komunální elektromobil N1/SS", 2], ["E.ON Energie, a.s.", 2], ["smart4smart s.r.o.", 2], ["ALUKOV a.s.", 2], ["X-scooters XS05 60V Li Extreme Dual 4000W", 2], ["LERA Motors s.r.o.", 2], ["Some company s.r.o.", 2], ["Some other company s.r.o.", 2], ["M Motors CZ s.r.o.", 3], ["Emil Frey ČR", 3], ["Užitkové elektromobily Goupil", 3], ["Volvo Group Czech Republic, s.r.o.", 3], ["Daimler Truck ČR", 3], ["Valeo", 3], ["Porsche Česká republika s.r.o.", 4], ["Škoda Auto", 4], ["Kia Czech s.r.o.", 4], ["Eco Rally", 5]];
 
 mapElements.forEach((mapElement, index) => {
@@ -21,4 +22,17 @@ mapElementsGroups.forEach((mapElementsGroup, index) => {
     })
 
     mapElementsGroups[index].innerHTML = newInnerHtml;
+})
+
+// mobile navbar
+const mobileNavbar = document.querySelector("#mobile-navbar");
+const hamburgerButton = document.querySelector("#hamburger-button");
+
+hamburgerButton.addEventListener("click", () => {
+    if (mobileNavbar.style.visibility == "hidden") return mobileNavbar.style.visibility = "visible";
+    mobileNavbar.style.visibility = "hidden";
+})
+
+addEventListener("resize", () => {
+    mobileNavbar.style.visibility = "hidden";
 })
